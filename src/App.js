759fb5baf650f0a7e87e1736d00dd9b0a159c19e
Ann3te <53444040+Ann3te@users.noreply.github.com>
2024-05-25 12:53:04 +0100

    Adding POLICE api functionality

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './component/Navbar'
-import Home from './component/Home'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Home from "./component/Home";
+import CrimeStats from "./component/CrimeStats";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* These are how comments are made in JSX! */}
         <Navbar />
-        <Home />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/stats" element={<CrimeStats />} />
+        </Routes>
       </header>
     </div>
   );
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,17 +1,32 @@
-import './Navbar.css'
-import 'bootstrap'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import "bootstrap";
 
-function Navbar () {
-    return (
-        <nav className="navBar">
-            <a href="#">logo</a>
-            <div className="topnav-right">
-                <a href="#stats">Stats</a>
-                <a href="#tips">Useful Tips</a>
-                <a href="#urgent-support">Urgent Support</a>
-            </div>
-        </nav>
-    )
+function Navbar() {
+  return (
+    <div>
+      <nav className="navBar p-4 bg-gray-800 text-white">
+        <Link to="/" className="text-xl font-bold">
+          logo
+        </Link>
+        <div className="topnav-right">
+          <Link to="/" className="ml-4">
+            Home
+          </Link>
+          <Link to="/stats" className="ml-4">
+            Stats
+          </Link>
+          <a href="#tips" className="ml-4">
+            Useful Tips
+          </a>
+          <a href="#urgent-support" className="ml-4">
+            Urgent Support
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
