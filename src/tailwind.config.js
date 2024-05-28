a9c418d085dbbe61f7c87bb37e584b6d8435052d
Ann3te <53444040+Ann3te@users.noreply.github.com>
2024-05-28 09:44:44 +0100

    Updating police-api branch

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,6 +1,8 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}", // Adjust the path according to your project structure
+    "./public/index.html", // Include other paths if necessary
+  ],
   theme: {
     extend: {},
   },
